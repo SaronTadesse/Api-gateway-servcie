8c504137aabc7c47e09ea4ac21105b32187702fe
sarontadesse <sarontadesse456@gmail.com>
2024-07-20 21:14:59 -0500

    initial commit

---
+++ src/main/java/saron_api_gateway_service/SaronApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package saron_api_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaronApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaronApiGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saron_api_gateway_service/SaronApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package saron_api_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaronApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
